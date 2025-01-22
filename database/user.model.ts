aad7f648952989afb5bfb50028df00a6a3e37e3a
arun sha <>
2025-01-22 16:59:09 +0530

    Created User Models

---
+++ database/user.model.ts
@@ -0,0 +1,33 @@
+import { model, models, Schema } from "mongoose";
+
+// 1. Create an interface representing a document in MongoDB.
+export interface IUser {
+  name: string;
+  username: string;
+  email: string;
+  bio?: string;
+  image: string;
+  location?: string;
+  portfolio?: string;
+  reputation?: number;
+}
+
+// 2. Create a Schema corresponding to the document interface.
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    bio: { type: String },
+    image: { type: String, required: true },
+    location: { type: String },
+    portfolio: { type: String },
+    reputation: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+// 3. Create a Model. or use the existing model
+const User = models?.user || model<IUser>("User", UserSchema);
+
+export default User;
