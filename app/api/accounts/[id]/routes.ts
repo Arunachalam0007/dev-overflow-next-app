51231347c7ad160dba4e657c12caa63f67940f8e
arun sha <>
2025-01-28 15:46:58 +0530

    Implemented Accounts Dynamic Routes

--- app/api/accounts/[id]/routes.ts
+++ app/api/accounts/[id]/routes.ts
@@ -1,12 +1,13 @@
+import Account from "@/database/account.model";
 import User from "@/database/user.model";
 import handleError from "@/lib/handlers/error";
 import { NotFoundError, ValidationError } from "@/lib/http-errors";
 import dbConnect from "@/lib/mongoose";
-import { UserSchema } from "@/lib/validation";
+import { AccountSchema, UserSchema } from "@/lib/validation";
 import { APIErrorResponse } from "@/types/global";
 import { NextResponse } from "next/server";
 
-// GET /api/users/[id] - Get a single user by ID
+// GET /api/users/[id] - Get a single Account by ID
 export async function GET(
   _: Request,
   { params }: { params: Promise<{ id: string }> }
@@ -15,27 +16,27 @@ export async function GET(
   const { id } = await params;
 
   // 2. Throw a NotFoundError if the ID is not provided
-  if (!id) throw new NotFoundError("User");
+  if (!id) throw new NotFoundError("Account");
   try {
     // 3. Connect to the database
     await dbConnect();
 
-    // 4. Find the user by ID
-    // or User.findOne({ _id: id });
-    const user = await User.findById(id);
+    // 4. Find the Account by ID
+    // or Account.findOne({ _id: id });
+    const account = await Account.findById(id);
 
-    // 5. Throw a NotFoundError if the user is not found
-    if (!user) throw new NotFoundError("User");
+    // 5. Throw a NotFoundError if the account is not found
+    if (!account) throw new NotFoundError("Account");
 
     //6. Return the user as a success response
-    return NextResponse.json({ success: true, data: user }, { status: 200 });
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
   } catch (error) {
     // 7. Handle the error using the custom error handler
     return handleError(error, "api") as APIErrorResponse;
   }
 }
 
-// DELETE /api/users/[id] - Delete a user by ID
+// DELETE /api/users/[id] - Delete a Account by ID
 export async function DELETE(
   _: Request,
   { params }: { params: Promise<{ id: string }> }
@@ -44,28 +45,28 @@ export async function DELETE(
   const { id } = await params;
 
   // 2. Throw a NotFoundError if the ID is not provided
-  if (!id) throw new NotFoundError("User");
+  if (!id) throw new NotFoundError("Account");
 
   try {
     // 3. Connect to the database
     await dbConnect();
 
-    // 4. Find the user by ID and delete it
+    // 4. Find the Account by ID and delete it
     // or User.findOneAndDelete({ _id: id });
-    const user = await User.findByIdAndDelete(id);
+    const account = await Account.findByIdAndDelete(id);
 
-    // 5. Throw a NotFoundError if the user is not found
-    if (!user) throw new NotFoundError("User");
+    // 5. Throw a NotFoundError if the Account is not found
+    if (!account) throw new NotFoundError("Account");
 
     // 6. Return a success response
-    return NextResponse.json({ success: true, data: user }, { status: 200 });
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
   } catch (error) {
     // 7. Handle the error using the custom error handler
     return handleError(error, "api") as APIErrorResponse;
   }
 }
 
-// PUT /api/users/[id] - Update a user by ID
+// PUT /api/users/[id] - Update a Account by ID
 export async function PUT(
   request: Request,
   { params }: { params: Promise<{ id: string }> }
@@ -74,7 +75,7 @@ export async function PUT(
   const { id } = await params;
 
   // 2. Throw a NotFoundError if the ID is not provided
-  if (!id) throw new NotFoundError("User");
+  if (!id) throw new NotFoundError("Account");
   try {
     // 3. Connect to the database
     await dbConnect();
@@ -82,17 +83,26 @@ export async function PUT(
 
     // 4. Validate the request body
     // Partial is used to allow partial updates
-    const validatedData = UserSchema.partial().parse(body);
+    const validatedData = AccountSchema.partial().safeParse(body);
 
-    // 5. Find the user by ID and update it
+    // 5. Throw a ValidationError if Validation fails
+    if (!validatedData.success)
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+
+    // 5. Find the Account by ID and update it
     // or User.findOneAndUpdate({ _id: id }, { $set: request.body }, { new: true });
-    const user = await User.findByIdAndUpdate(id, validatedData, { new: true });
+    const updatedAccount = await Account.findByIdAndUpdate(id, validatedData, {
+      new: true,
+    });
 
     // 6. Throw a NotFoundError if the user is not found
-    if (!user) throw new NotFoundError("User");
+    if (!updatedAccount) throw new NotFoundError("Account");
 
     // 7. Return the updated user as a success response
-    return NextResponse.json({ success: true, data: user }, { status: 200 });
+    return NextResponse.json(
+      { success: true, data: updatedAccount },
+      { status: 200 }
+    );
   } catch (error) {
     // 8. Handle the error using the custom error handler
     return handleError(error, "api") as APIErrorResponse;
